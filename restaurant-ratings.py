3ca40b32ac248d176127d64616a208dddd2ccdd4
Rosie <rosie@riveter18a-md.swna.wdpr.disney.com>
2016-04-11 17:29:56 -0700

    #4 added user prompt and title() method

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -3,22 +3,30 @@ import sys
 def sort_restaurant_ratings():
 # 1 open file
 
-	file_name = sys.argv[1]
+    file_name = sys.argv[1]
 
 
 # 2 create a dictionary (Name:Rating) from file
-	restaurant_dict = {}
-	
-	for line in open(file_name):
-		line = line.rstrip()
-		words = line.split(":")	
-		restaurant_dict[words[0]] = words[1]
+    restaurant_dict = {}
+    
+    for line in open(file_name):
+        line = line.rstrip()
+        words = line.split(":") 
+        restaurant_dict[words[0]] = words[1]
 
-# 3 sort restaurant names alphabetically then call the associated values
+# 3 sort restaurant names alphabetically and print w/rating
 
-	for key, value in sorted(restaurant_dict.items()):
-# 4 print sorted dictionary (alphabetical order)
-		print "{} has a rating of {}." .format(key, value)
+    # for key, value in sorted(restaurant_dict.items()):
+    #   print "{} has a rating of {}." .format(key, value)
 
+# 4 Prompt User for restaurant name and score(integer) - raw_input
+    restaurant_name = raw_input("Please enter restaurant name: ")
+    restaurant_rating = raw_input("Please enter the restaurant's rating: ")
+
+# 5 Store new restaurant/rating in the dictionary
+    restaurant_dict[restaurant_name.title()] = restaurant_rating
+# 6 Print all of the rating in alphabetical order
+    for key, value in sorted(restaurant_dict.items()):
+        print "{} has a rating of {}." .format(key, value)
 
 sort_restaurant_ratings()
